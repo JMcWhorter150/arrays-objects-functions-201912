9c05c6f59dbd23f90159f14976cae3887fd79152
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-04 13:42:13 -0500

    Adds cool city function

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,7 @@ function test_1(fN) {
     array = [-10, -5, 6, 7, 10000, 0, 0.1, -0.1, -1000]
     return fN(array);
 }
+// created test array with negatives, positives, floats, odds, and evens
 
 // console.log(test_1(getPositiveNumbers));
 // worked :)
@@ -38,3 +39,32 @@ function getSquaredNumbers(arrayOfNumbers) {
 
 // console.log(test_1(getSquaredNumbers));
 // worked :)
+
+function testCities(fN) {
+    const cities = [
+        { name: 'Los Angeles', temperature: 60.0},
+        { name: 'Atlanta', temperature: 52.0 },
+        { name: 'Detroit', temperature: 48.0 },
+        { name: 'New York', temperature: 80.0 }
+      ];
+    return fN(cities);
+}
+// creates test for cities example
+
+function cityTempLessThan70(cityObj) {
+    let cityTemp = cityObj.temperature
+    return (cityTemp < 70);
+}
+
+function getCoolCities(arrayOfCityObjs) {
+    // setup
+    // I want to for each object in cities to check if the temperature of the city is less than 70 and filter out cities with temp greater than 70
+    // Then I want to return an array of only the city objects
+    // To do this, I'm going to use a filter function due to needing a smaller list
+    // I need the filter to take in a city object, get the temp from each city, and then return true or false depending on if the city temp is less than 70
+    const coolCities = arrayOfCityObjs.filter(cityTempLessThan70);
+    return coolCities;
+}
+
+// console.log(testCities(getCoolCities));
+// works :)

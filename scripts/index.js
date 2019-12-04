1f4e7827865194ad1d222b4d9fb2ddb877eadc48
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-04 13:15:51 -0500

    Adds positive number function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 // Your code goes here!
+
+function isPositive(number) {
+    return (number >= 0);
+}
+
+function getPositiveNumbers(arrayOfNumbers) {
+    const positiveArrayOfNumbers = arrayOfNumbers.filter(isPositive);
+    return positiveArrayOfNumbers;
+}
+
+function test_1(fN) {
+    array = [-10, -5, 5, 7, 10000, 0, 0.1, -0.1, -1000]
+    return fN(array);
+}
+
+// console.log(test_1(getPositiveNumbers));
+
